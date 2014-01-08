85b157a03c0cdb011eeb1ec54d6a8db189445a48
averangeall <averangeall@gmail.com>
2014-01-08 11:31:58 +0800

    Find the least filled OCR groundtruth

--- ocr/views.py
+++ ocr/views.py
@@ -7,17 +7,23 @@ from turker import models
 def write(request):
     dictt = {}
     dictt.update(csrf(request))
-    templates = models.Meme.objects.filter(first_line='', second_line='').values('template').annotate()
-    len(templates) # removing this line will cause templates.count() error (django bug?)
-    assert templates.count()
-    template_choice = random.randint(0, templates.count() - 1)
-    template = models.Template.objects.get(id=templates[template_choice]['template'])
-    memes = models.Meme.objects.filter(template=template)
-    assert memes.count()
-    meme_choice = random.randint(0, memes.count() - 1)
-    meme = memes[meme_choice]
-    assert meme.first_line == '' and meme.second_line == ''
-    dictt['meme'] = {'template': template.name, 'gag_id': meme.gag_id}
+
+    templates = models.Template.objects.filter(expect_suitable=True)
+    len_memes = models.Meme.objects.all().count()
+    sorted_memes = [[] for i in range(len_memes)]
+    for template in templates:
+        empty_memes = models.Meme.objects.filter(template=template).filter(first_line='').filter(second_line='')
+        fill_memes = models.Meme.objects.filter(template=template).exclude(first_line='').exclude(second_line='')
+        sorted_memes[fill_memes.count()].extend(empty_memes)
+
+    for memes in sorted_memes:
+        if not memes:
+            continue
+        meme_choice = random.randint(0, len(memes) - 1)
+        meme = memes[meme_choice]
+        dictt['meme'] = {'template': meme.template.name, 'gag_id': meme.gag_id}
+        break
+
     return render_to_response('write.html', dictt)
 
 def insert(request):
